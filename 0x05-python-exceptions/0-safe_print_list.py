7e05a39f8992a41fd8d5fa620e780f45938da03a
Akram-Adam <akramadam050@gmail.com>
2023-12-20 19:01:04 -0800

    Akram

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,6 @@
-afe_print_list(my_list=[], x=0):
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
     """
     Prints x elements of a list.
     All elements must be printed on the same line followed by a new line.
