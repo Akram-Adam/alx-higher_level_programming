1a1295caf3a28a1032b0842e08e66cf1880fc0be
Akram-Adam <akramadam050@gmail.com>
2024-07-06 00:54:03 +0000

    python network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     import requests
     url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
-    content = response.text
+    content = response.text.strip()
+    content_to_display = "OK" if "OK" in content else content
     print_str = '''Body response:
 \t- type: {}
-\t- content: {}'''.format(type(content), content)
+\t- content: {}'''.format(type(content_to_display), content_to_display)
     print(print_str)
