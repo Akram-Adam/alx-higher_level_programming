648083c1d503b1212ac0bed67b59834358d86790
Akram-Adam <akramadam050@gmail.com>
2024-07-06 00:40:25 +0000

    python network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
 """
-Fetches the GitHub user ID using Basic
-Authentication with a personal access token.
-Usage: ./github_user_id.py <username> <personal_access_token>
-"""
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    access_token = sys.argv[2]
-
-    url = f"https://api.github.com/user"
-    headers = {
-        "Authorization": f"Basic {username}:{access_token}"
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            user_data = response.json()
-            user_id = user_data['id']
-            print(f"Your GitHub user ID is: {user_id}")
-        else:
-            print(f"Failed: {response.status_code} - {response.text}")
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
