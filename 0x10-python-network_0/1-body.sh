90e3db04863870e1340a3b53700f8c11d6f4bdbd
Akram-Adam <akramadam050@gmail.com>
2024-07-06 19:34:02 +0000

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# التحقق من تمرير العنوان URL كمعامل
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# تعيين العنوان URL إلى متغير
-URL="$1"
-
-# إرسال طلب GET باستخدام curl
-response=$(curl -s -w "%{http_code}" "$URL")
-
-# استخراج رمز الحالة ومحتوى الاستجابة
-http_code=${response: -3}
-body=${response:0: -3}
-
-# عرض محتوى الاستجابة إذا كان رمز الحالة 200
-if [ "$http_code" -eq 200 ]; then
-  echo "$body"
-else
-  echo "HTTP status code: $http_code"
-fi
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sX GET $1 -L
