6f7cb846513859948d41c4aa99d1f7b4664b92a2
Akram-Adam <akramadam050@gmail.com>
2024-07-30 22:52:39 +0000

    node.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Usage: node script.js <file_path>');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
+
+
+
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
